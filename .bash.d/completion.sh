4717fb0b3072b5b5c6ae9f28fd62cdf58d83aee7
Ches Martin <ches.martin@gmail.com>
2009-09-10 12:29:31 -0400

    bash: disable ssh completion for known hosts since they're hashed now for security

--- .bash.d/completion.sh
+++ .bash.d/completion.sh
@@ -36,14 +36,14 @@ complete -o default -o nospace -F _thorcomplete thor
 #
 # SSH
 #
-SSH_KNOWN_HOSTS=( $(cat ~/.ssh/known_hosts | \
-  cut -f 1 -d ' ' | \
-  sed -e s/,.*//g | \
-  uniq | \
-  egrep -v [0123456789]) )
+#SSH_KNOWN_HOSTS=( $(cat ~/.ssh/known_hosts | \
+#  cut -f 1 -d ' ' | \
+#  sed -e s/,.*//g | \
+#  uniq | \
+#  egrep -v [0123456789]) )
 SSH_CONFIG_HOSTS=( $(cat ~/.ssh/config | grep "Host " | grep -v "*" | cut -f 2 -d ' ') )
  
-complete -o default -W "${SSH_KNOWN_HOSTS[*]} ${SSH_CONFIG_HOSTS[*]}" ssh
+complete -o default -W "${SSH_CONFIG_HOSTS[*]}" ssh
 
 # enable command and file completion after sudo
 complete -f -c sudo
