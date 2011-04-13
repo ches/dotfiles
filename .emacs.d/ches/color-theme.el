e7e17432107b1e7adf4e8cbf05fee23f75cef6cb
Ches Martin <ches@whiskeyandgrits.net>
2011-04-13 18:15:15 +0700

    emacs: Twilight color theme

--- .emacs.d/ches/color-theme.el
+++ .emacs.d/ches/color-theme.el
@@ -1,10 +1,12 @@
 ;; Look at the pretty colors!
 (color-theme-initialize)
-(vendor 'color-theme-ir-black)
-(color-theme-ir-black)
+(vendor 'color-theme-twilight)
+(color-theme-twilight)
 
 ;; Decent:
 ; (color-theme-clarity)
+; (vendor 'color-theme-ir-black)
+; (color-theme-ir-black)
 
 ;; These are terrible, I just need obvious examples to start customizing...
 (set-face-background 'mmm-output-submode-face  "royalblue")
