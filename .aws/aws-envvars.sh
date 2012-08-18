eefbc2d2f4f8c38926b50031d13db884cd68c6f9
Ches Martin <ches@whiskeyandgrits.net>
2012-08-18 19:30:45 +0700

    Update AWS envvars for homebrew's versioned directory change

--- .aws/aws-envvars.sh
+++ .aws/aws-envvars.sh
@@ -5,11 +5,11 @@ export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
 export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
 
 if [ "$(uname -s)" == "Darwin" ]; then
-    export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.3/jars"
+    export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
     export AWS_IAM_HOME="/usr/local/Cellar/aws-iam-tools/HEAD/jars"
     export AWS_CLOUDWATCH_HOME="/usr/local/Cellar/cloud-watch/1.0.12.1/jars"
-    export AWS_AUTO_SCALING_HOME="/usr/local/Cellar/auto-scaling/1.0.49.1/jars"
-    export AWS_CLOUDFORMATION_HOME="/usr/local/Cellar/aws-cfn-tools/1.0.8/jars"
+    export AWS_AUTO_SCALING_HOME="/usr/local/Library/LinkedKegs/auto-scaling/jars"
+    export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
 fi
 
 export SERVICE_HOME="$AWS_CLOUDWATCH_HOME"
