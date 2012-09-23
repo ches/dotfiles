52f77a87aafef8a0090f4321b9a4f4fc157a4d0d
Ches Martin <ches@whiskeyandgrits.net>
2012-09-23 23:04:23 +0700

    AWS CloudWatch tools finally has LinkedKegs path
    --HG--
    extra : rebase_source : 544bddc13e7d22b818edb194b0fabe59570a3488

--- .aws/aws-envvars.sh
+++ .aws/aws-envvars.sh
@@ -7,7 +7,7 @@ export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
 if [ "$(uname -s)" == "Darwin" ]; then
     export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
     export AWS_IAM_HOME="/usr/local/Cellar/aws-iam-tools/HEAD/jars"
-    export AWS_CLOUDWATCH_HOME="/usr/local/Cellar/cloud-watch/1.0.12.1/jars"
+    export AWS_CLOUDWATCH_HOME="/usr/local/Library/LinkedKegs/cloud-watch/jars"
     export AWS_AUTO_SCALING_HOME="/usr/local/Library/LinkedKegs/auto-scaling/jars"
     export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
 fi
