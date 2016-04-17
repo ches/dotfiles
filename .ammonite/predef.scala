3380609d9e7504d4db265f88a6668ce99a5aec7f
Ches Martin <ches@whiskeyandgrits.net>
2016-04-18 00:56:18 +0700

    ammonite: Show compiler warnings in the REPL

--- .ammonite/predef.scala
+++ .ammonite/predef.scala
@@ -1,2 +1,7 @@
+// See http://www.lihaoyi.com/Ammonite/#Configuration
+
 // Not really digging the @ sign prompt.
 repl.prompt() = "> "
+
+// Love the double negative... enable compiler warnings
+compiler.settings.nowarnings.value = false
