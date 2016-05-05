d10f6b5e533a8957c66a34e5f4a019c4f5eae2db
Ches Martin <ches@whiskeyandgrits.net>
2016-05-05 18:49:19 +0700

    bash: Remove Docker env var settings for boot2docker
    Now using Docker Machine with `docker-machine env docker-vm`, or Docker
    for Mac beta!

--- .bash.d/platform-osx.sh
+++ .bash.d/platform-osx.sh
@@ -11,10 +11,6 @@ fi
 # =       App-specific Bits       =
 # =================================
 
-export DOCKER_HOST=tcp://192.168.59.103:2375  # for boot2docker
-export DOCKER_TLS_VERIFY=1
-export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
 export JAVA_HOME=$(/usr/libexec/java_home --version 1.8.0)
 export NODE_PATH="/usr/local/lib/node_modules"
