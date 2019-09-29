3c94f711e4ad84d34b669b30d6bdb8f0c16e7c03
Ches Martin <ches@whiskeyandgrits.net>
2019-09-29 17:19:45 +0700

    ptpython: Try to catch prompt_toolkit recent API breakages

--- .ptpython/config.py
+++ .ptpython/config.py
@@ -8,7 +8,10 @@ Some reminders:
 """
 from __future__ import unicode_literals
 from prompt_toolkit.filters import ViInsertMode
-from prompt_toolkit.key_binding.input_processor import KeyPress
+try:
+    from prompt_toolkit.key_binding.key_processor import KeyPress
+except ImportError:
+    from prompt_toolkit.key_binding.input_processor import KeyPress
 from prompt_toolkit.keys import Keys
 from pygments.token import Token
 
